09bbaa075da4e72bdae4b63aee489014e46ab740
Bhavin Katabathuni <bhavin2897@gmail.com>
2022-04-04 14:13:39 +0200

    Initial commit

---
+++ ckanext/wikidata/plugin.py
@@ -0,0 +1,43 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+from flask import Blueprint
+from ckan.common import request, config
+from ckanext.wikidata.controller.wikidata import WikidataController
+
+
+class WikidataPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
+    plugins.implements(plugins.IBlueprint)
+
+    # IConfigurer
+    def update_config(self, config):
+        toolkit.add_template_directory(config, 'templates')
+        toolkit.add_public_directory(config, 'public')
+        toolkit.add_resource('public/statics', 'ckanext-wikidata')
+
+
+    def get_blueprint(self):
+
+        blueprint = Blueprint(self.name, self.__module__)
+
+        blueprint.template_folder = u'templates'
+        blueprint.add_url_rule(
+            u'/localhost:5000/fancy_type/<package_name>',
+            u'generate_wikilink',
+            WikidataController.generate_wikilink,
+            methods=['POST']
+        )
+        ## To display the information
+        blueprint.template_folder = u'templates'
+        blueprint.add_url_rule(
+            u'/localhost:5000/fancy_type/<package_name>',
+            u'generate_wikilink',
+            WikidataController.generate_wikilink,
+            methods=['GET']
+        )
+
+        return blueprint
+
+    def get_helpers(self):
+       return {'wikidata_link': WikidataController.generate_wikilink}
